2f1a64dbe03133ac6ff84b279cf67af0c50496cc
FurkanYahsi <furkanyahsi103@gmail.com>
2025-07-22 14:18:56 +0300

    Adding a recipe has become a two-step process. The user will choose which category and type the recipe belongs to.

--- src/components/CreateRecipeForm/CreateRecipeForm.logic.tsx
+++ src/components/CreateRecipeForm/CreateRecipeForm.logic.tsx
@@ -1,90 +1,172 @@
-import React, { FormEvent, useEffect, useRef } from 'react'
+import React, { FormEvent, useEffect, useRef, useState } from 'react'
 import { createRecipe } from '../../services/RecipeServices/RecipeService.export';
 import { ToastMessage } from '../../utils/ToastMessage/ToastMessage';
 
 const useCreateRecipeForm = () => {
 
-    const { contextHolder, showNotification } = ToastMessage();
+  const { contextHolder, showNotification } = ToastMessage();
+  const recipeNameRef = useRef<HTMLTextAreaElement>(null);
+  const recipeStoryRef = useRef<HTMLTextAreaElement>(null);
+  const ingredientsRef = useRef<HTMLTextAreaElement>(null);
+  const instructionsRef = useRef<HTMLTextAreaElement>(null);
+  const [whichStep, setWhichStep] = useState(1);
+  const [inputs, setInputs] = useState<string[]>([]);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+  const [typeOptions, setTypeOptions] = useState<string[]>([]);
 
-    const recipeNameRef = useRef<HTMLTextAreaElement>(null);
-    const recipeStoryRef = useRef<HTMLTextAreaElement>(null);
-    const ingredientsRef = useRef<HTMLTextAreaElement>(null);
-    const instructionsRef = useRef<HTMLTextAreaElement>(null);
-    
-    const adjustHeight = (element: HTMLTextAreaElement | null) => {
-      if (element) {
-        element.style.height = 'auto'; // Reset height to auto to shrink if needed
-        element.style.height = `${element.scrollHeight}px`; // Set height to scrollHeight
+  useEffect(() => {
+    if (selectedCategories.length > 0) {
+      setTypeOptions([]); // Reset type options when categories change
+      if (selectedCategories.includes("Meat Dishes")) {
+        setTypeOptions([...typeOptions, "Grilled Meatball", "Fish & Seafood", "Chicken", "Hamburgers"]);
+      } 
+      if (selectedCategories.includes("Vegetable Dishes")) {
+        setTypeOptions([...typeOptions, "Stir-fried Vegetables", "Roasted Vegetables", "Steamed Vegetables"]);
+      } 
+      if (selectedCategories.includes("Pasta Recipes")) {
+        setTypeOptions([...typeOptions, "Spaghetti Bolognese", "Penne Arrabbiata", "Fettuccine Alfredo"]);
+      } 
+      if (selectedCategories.includes("Pizza Recipes")) {
+        setTypeOptions([...typeOptions, "Margherita Pizza", "Pepperoni Pizza", "Vegetarian Pizza"]);
+      } 
+      if (selectedCategories.includes("Soup")) {
+        setTypeOptions([...typeOptions, "Chicken Soup", "Vegetable Soup", "Mushroom Soup"]);
       }
-    };
-  
-    const adjustAllTextareas = () => {
-      adjustHeight(recipeNameRef.current);
-      adjustHeight(recipeStoryRef.current);
-      adjustHeight(ingredientsRef.current);
-      adjustHeight(instructionsRef.current);
+      if (selectedCategories.includes("Desserts")) {
+        setTypeOptions([...typeOptions, "Cakes", "Milk Dessert", "Ice Cream", "Fruit Recipes"]);
+      } 
+      if (selectedCategories.includes("Salads")) {
+        setTypeOptions([...typeOptions, "Caesar Salad", "Greek Salad", "Caprese Salad"]);
+      }
+      if (selectedCategories.includes("Beverages")) {
+        setTypeOptions([...typeOptions, "Cold Beverages", "Hot Beverages"]);
+      }
+      if (selectedCategories.includes("Snacks")) {
+        setTypeOptions([...typeOptions, "Chips", "Popcorn", "Nuts"]);
+      }
+      if (selectedCategories.includes("Other Recipes")) {
+        setTypeOptions([...typeOptions, "Rice Dishes", "Snacks", "Egg Dishes", "Legume Dishes", "Bread Recipes"]);
+      }
     }
+  }, [selectedCategories]);
   
-    useEffect(() => {
+  const adjustHeight = (element: HTMLTextAreaElement | null) => {
+    if (element) {
+      element.style.height = 'auto'; // Reset height to auto to shrink if needed
+      element.style.height = `${element.scrollHeight}px`; // Set height to scrollHeight
+    }
+  };
+
+  const adjustAllTextareas = () => {
+    adjustHeight(recipeNameRef.current);
+    adjustHeight(recipeStoryRef.current);
+    adjustHeight(ingredientsRef.current);
+    adjustHeight(instructionsRef.current);
+  }
+
+  useEffect(() => {
+    adjustAllTextareas();
+  }, []);
+
+  const handleInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    adjustHeight(e.currentTarget);
+  };
+
+  const handleFirstNextStep = (e : FormEvent<HTMLFormElement>) => {
+      e.preventDefault(); // Do not refresh the page
+      if (!recipeNameRef.current?.value ||!ingredientsRef.current?.value || !instructionsRef.current?.value) {
+        showNotification("Please fill in all fields before sharing the recipe.", "error");
+        return;
+      }
+      if (recipeNameRef.current?.value.length < 3) {
+        showNotification("Recipe name must be at least 3 characters long.", "error");
+        return;
+      }
+       if (ingredientsRef.current?.value.length < 5) {
+        showNotification("Ingredients must be at least 5 characters long.", "error");
+        return;
+      }
+       if (instructionsRef.current?.value.length < 20) {
+        showNotification("Instructions must be at least 20 characters long.", "error");
+        return;
+      }
+      inputs[0] = recipeNameRef.current?.value || '';
+      inputs[1] = recipeStoryRef.current?.value || '';
+      inputs[2] = ingredientsRef.current?.value || '';
+      inputs[3] = instructionsRef.current?.value || '';
+
+      setWhichStep(whichStep + 1);
+  }
+
+  useEffect(() => {
+  // When the user navigates back to step 1, update the textareas with the saved inputs
+    if (whichStep === 1 && inputs.length > 0) {
+      if (recipeNameRef.current) recipeNameRef.current.value = inputs[0] || '';
+      if (recipeStoryRef.current) recipeStoryRef.current.value = inputs[1] || '';
+      if (ingredientsRef.current) ingredientsRef.current.value = inputs[2] || '';
+      if (instructionsRef.current) instructionsRef.current.value = inputs[3] || '';
+
       adjustAllTextareas();
-    }, []);
-  
-    const handleInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
-      adjustHeight(e.currentTarget);
-    };
-  
-    const handleShareRecipe = (e : FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // Do not refresh the page
-        if (!recipeNameRef.current?.value ||!ingredientsRef.current?.value || !instructionsRef.current?.value) {
-          showNotification("Please fill in all fields before sharing the recipe.", "error");
-          return;
-        }
-
-        if (recipeNameRef.current?.value.length < 3) {
-          showNotification("Recipe name must be at least 3 characters long.", "error");
-          return;
-        }
-
-         if (ingredientsRef.current?.value.length < 5) {
-          showNotification("Ingredients must be at least 5 characters long.", "error");
-          return;
-        }
-
-         if (instructionsRef.current?.value.length < 20) {
-          showNotification("Instructions must be at least 20 characters long.", "error");
-          return;
-        }
-        
-        createRecipe({
-            recipe_name: recipeNameRef.current?.value,
-            recipe_story: recipeStoryRef.current?.value,
-            recipe_ingredients: ingredientsRef.current?.value,
-            recipe_instructions: instructionsRef.current?.value,
-        }).then((response: any) => {
-            if (response && response.success) {
-                showNotification("Recipe shared successfully!", "success");
-                // Reset the form fields
-                if (recipeNameRef.current) recipeNameRef.current.value = '';
-                if (recipeStoryRef.current) recipeStoryRef.current.value = '';
-                if (ingredientsRef.current) ingredientsRef.current.value = '';
-                if (instructionsRef.current) instructionsRef.current.value = '';
-                adjustAllTextareas(); // Adjust height again
-            } else {
-                // console.error(response?.errorMessage || "Failed to create recipe!");
-            }
-        }).catch((error: any) => {
-            console.error("Error creating recipe:", error);
-        });
     }
-    return {
-      handleShareRecipe,
-      recipeNameRef,
-      recipeStoryRef,
-      ingredientsRef,
-      instructionsRef,
-      handleInput,
-      contextHolder
+    if (whichStep === 1) { 
+      setSelectedCategories([]);
+      setSelectedTypes([]);
     }
+  }, [whichStep]);
+
+  const handleShareRecipe = () => {
+
+    if (selectedCategories.length === 0) {
+      showNotification("Please select at least one category.", "error");
+      return;
+    }
+    if (selectedTypes.length === 0) {
+      showNotification("Please select at least one type.", "error");
+      return;
+    }
+
+    if (inputs.length === 4 && inputs.every(input => input.trim() !== '') && selectedCategories.length > 0 && selectedTypes.length > 0) {
+      createRecipe({
+        recipe_name: inputs[0],
+        recipe_story: inputs[1],
+        recipe_ingredients: inputs[2],
+        recipe_instructions: inputs[3],
+        category: selectedCategories,
+        type: selectedTypes,
+      }).then((response: any) => {
+        if (response && response.success) {
+          showNotification("Recipe shared successfully!", "success");
+          setSelectedCategories([]);
+          setSelectedTypes([]);
+          inputs.length = 0; // Clear inputs after successful submission
+        } else {
+           showNotification("Failed to create recipe! " + (response?.errorMessage || ""), "error");
+           console.error(response?.errorMessage || "Failed to create recipe!");
+        }
+      }).catch((error: any) => {
+        console.error("Error creating recipe:", error);
+        showNotification("Error creating recipe! Please try again later.", "error");
+      });
+    }      
+  }
+  return {
+    handleFirstNextStep,
+    handleShareRecipe,
+    whichStep,
+    setWhichStep,
+    recipeNameRef,
+    recipeStoryRef,
+    ingredientsRef,
+    instructionsRef,
+    handleInput,
+    contextHolder,
+    selectedCategories,
+    setSelectedCategories,
+    selectedTypes,
+    setSelectedTypes,
+    typeOptions,
+  }
 }
 
 export default useCreateRecipeForm
--- src/components/CreateRecipeForm/CreateRecipeForm.tsx
+++ src/components/CreateRecipeForm/CreateRecipeForm.tsx
@@ -1,35 +1,84 @@
 import './CreateRecipeForm.css';
 import useCreateRecipeForm from './CreateRecipeForm.logic';
+import MultipleSelectDropDown from '../MultipleSelectDropDown/MultipleSelectDropDown';
+
+import { IconBaseProps } from 'react-icons';
+import { HiMiniArrowLeft } from "react-icons/hi2";
+import { HiMiniArrowRight } from "react-icons/hi2";
+
+const IconArrowLeft = HiMiniArrowLeft as React.FC<IconBaseProps>;
+const IconArrowRight = HiMiniArrowRight as React.FC<IconBaseProps>;
 
 const CreateRecipeForm = () => {
 
-  const { handleShareRecipe, recipeNameRef, recipeStoryRef,ingredientsRef, instructionsRef, handleInput, contextHolder } = useCreateRecipeForm();
+  const { whichStep,
+          setWhichStep,
+          handleFirstNextStep,
+          handleShareRecipe,
+          recipeNameRef,
+          recipeStoryRef,
+          ingredientsRef,
+          instructionsRef,
+          handleInput,
+          contextHolder,
+          selectedCategories,
+          setSelectedCategories,
+          selectedTypes,
+          setSelectedTypes,
+          typeOptions } = useCreateRecipeForm();
+          
 
-  return (      
-    <div className="send-recipe-container">
-      {contextHolder}
-      <form onSubmit={handleShareRecipe}>      
-        <div className='mini-container'>
-          <div className='header'>Recipe Name</div>
-          <textarea ref={recipeNameRef} onInput={handleInput} rows={1} name='recipeName'/>
+  return (
+    <>
+    {contextHolder}
+      {whichStep === 1 &&
+        <div className="send-recipe-container">          
+          <form onSubmit={handleFirstNextStep}>
+            <div className='mini-container'>
+              <div className='header'>Recipe Name</div>
+              <textarea ref={recipeNameRef} onInput={handleInput} rows={1} name='recipeName'/>
+            </div>
+            <div className='mini-container'>
+              <div className='header'>Recipe History</div>
+              <textarea ref={recipeStoryRef} onInput={handleInput} rows={3} name='recipeStory'/>
+            </div>
+            <div className='mini-container'>          
+              <div className='header'>Ingredients</div>
+              <textarea placeholder="Write all the ingredients under each other" ref={ingredientsRef} onInput={handleInput} rows={5} name='recipeIngredients'/>
+            </div>
+            <div className='mini-container'>
+              <div className='header'>Instructions</div>
+              <textarea placeholder="Write all the preparation steps" ref={instructionsRef} onInput={handleInput} rows={5} name='recipeInstructions'/>
+            </div>
+            <div className="button-container">
+              <button type='submit' className='button-share-recipe'>Next Step <IconArrowRight/></button>
+            </div>
+          </form>
         </div>
-        <div className='mini-container'>
-          <div className='header'>Recipe History</div>
-          <textarea ref={recipeStoryRef} onInput={handleInput} rows={3} name='recipeStory'/>
+      }
+      {whichStep === 2 &&
+        <div className="send-recipe-container">
+          <div className='mini-container'>
+          <MultipleSelectDropDown 
+            header="Recipe Category"
+            options={["Meat Dishes", "Vegetable Dishes", "Pasta Recipes", "Pizza Recipes", "Soup", "Desserts", "Salads", "Beverages", "Snacks", "Other Recipes"]}
+            selectedOptions={selectedCategories}
+            setSelectedOptions={setSelectedCategories}
+          />
+          <MultipleSelectDropDown 
+            header="Recipe Type"
+            options={typeOptions}
+            selectedOptions={selectedTypes}
+            setSelectedOptions={setSelectedTypes}
+          />
+          </div>
+          <div className="button-container">
+            <button onClick={() => setWhichStep(1)} className='previous-page create-recipe-buttons-width'><IconArrowLeft/>Back</button>
+            <button onClick={handleShareRecipe} className='next-page create-recipe-buttons-width'>Share Recipe<IconArrowRight/></button>
+          </div>
         </div>
-        <div className='mini-container'>          
-          <div className='header'>Ingredients</div>
-          <textarea placeholder="Write all the ingredients under each other" ref={ingredientsRef} onInput={handleInput} rows={5} name='recipeIngredients'/>
-        </div>
-        <div className='mini-container'>
-          <div className='header'>Instructions</div>
-          <textarea placeholder="Write all the preparation steps" ref={instructionsRef} onInput={handleInput} rows={5} name='recipeInstructions'/>
-        </div>
-        <div className="button-container">
-          <button type='submit' className='button-share-recipe'>Share Recipe</button>
-        </div>
-      </form>
-    </div>    
+      }
+    </>
   )
 }
 

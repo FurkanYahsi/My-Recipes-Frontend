0e632cf7ebc963593e48821998ec2b304f363186
FurkanYahsi <furkanyahsi103@gmail.com>
2025-07-23 09:52:23 +0300

    DropDownMenuElement has different props for select the categories and the types.

--- src/components/DropdownMenuElement/DropdownMenuElement.tsx
+++ src/components/DropdownMenuElement/DropdownMenuElement.tsx
@@ -1,12 +1,42 @@
 import './DropdownMenuElement.css';
 
+import { IconBaseProps } from "react-icons";
+import { MdDone } from "react-icons/md";
+
+const IconDone = MdDone as React.FC<IconBaseProps>;
+
 interface DropdownMenuElementProps {
     header: string;
     items: string[];
     img?: string;
+    selectedCategories?: string[];
+    setSelectedCategories?: (options: string[]) => void;
+    selectedTypes?: string[];
+    setSelectedTypes?: (options: string[]) => void;
 }
 
-const DropdownMenuElement: React.FC<DropdownMenuElementProps> = ({header, items, img}) => {
+const DropdownMenuElement: React.FC<DropdownMenuElementProps> = ({header, items, img, selectedCategories, setSelectedCategories, selectedTypes, setSelectedTypes}) => {
+
+    const toggleCategory = (category: string) => {
+        if (selectedCategories && setSelectedCategories) {
+            if (selectedCategories.includes(category)) {
+                setSelectedCategories(selectedCategories.filter(cat => cat !== category));
+            } else {
+                setSelectedCategories([...selectedCategories, category]);
+            }
+        }
+    };
+
+    const toggleType = (type: string) => {
+        if (selectedTypes && setSelectedTypes) {
+            if (selectedTypes.includes(type)) {
+                setSelectedTypes(selectedTypes.filter(t => t !== type));
+            } else {
+                setSelectedTypes([...selectedTypes, type]);
+            }
+        }
+    };
+
   return (
     <>
         <div className='main-wrapper'>
@@ -17,15 +47,21 @@ const DropdownMenuElement: React.FC<DropdownMenuElementProps> = ({header, items,
 
             <div className='content'>
 
-                <div className='header'>
-                    {header}      
+                <div className='header' onClick={() => toggleCategory(header)}>
+                    {header}
+                    {selectedCategories && selectedCategories.includes(header) ? <IconDone style={{color:'green'}}/> : ''}                       
                 </div>
                 
                 <div className='items-wrapper'>
                     {items.map((item, index) => {
                         return (
-                            <div key={index} className="items">
+                            <div key={index} className="items" onClick={() => toggleType(item)}>
                                 {item}
+                                <span className="check-icon">
+                                    {selectedTypes && selectedTypes.includes(item) && 
+                                        <IconDone style={{color:'green'}}/>
+                                    }
+                                </span>     
                             </div>
                         )
                     })}

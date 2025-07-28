854e57ec88a0110dc2d35b86d60c1ec6498066bd
FurkanYahsi <furkanyahsi103@gmail.com>
2025-07-28 13:19:49 +0300

    Minor change: The content texts of home screen were changed.

--- src/components/Contents/Contents.tsx
+++ src/components/Contents/Contents.tsx
@@ -16,16 +16,16 @@ const Contents = () => {
   
   return (
     <div className='contents'>
-      <div className='type-of-recipe'>Type of recipe</div>
-      <div className='title'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
-      <div className='comment'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut commodi deserunt ipsum, quidem totam repellat. Dolore, nobis vel esse dolorem.</div>
+      <div className='type-of-recipe'>Main Course</div>
+      <div className='title'>Fettuccine with Bolognese Sauce</div>
+      <div className='comment'>Here's a quick and easy recipe for spaghetti with bolognese sauce, both delicious and easy to make. I highly recommend you try this recipe, which perfectly complements spaghetti and makes it a delicious dish you won't be able to get enough of.</div>
       <div className='buttons'>
         <ButtonPrimary name='View Recipe'/>
         <button onClick={() => setIsLiked(!isLiked)} id='primary-2'>{isLiked ? <HeartIconFilled/> : <HeartIcon/>}</button>
       </div>
       <div className='owner-profile'>
-        <div className='recipe-title'>Recipe Title</div>
-        <div className='user-profile'>User Profile</div>
+        <div className='recipe-title'>Fettuccine with Bolognese Sauce</div>
+        <div className='user-profile'>Nefis Yemek Tarifleri</div>
         <div className='dashed-lines'>
           <div className="dashed-line-1"></div>
           <div className="dashed-line-2"></div>

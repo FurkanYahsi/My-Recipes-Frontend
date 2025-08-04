aa76fb5e724f22f2c5379f40ff1fb1efd18a514d
FurkanYahsi <furkanyahsi103@gmail.com>
2025-08-04 14:00:24 +0300

    User role services were created.

--- src/services/AuthServices/AuthService.export.tsx
+++ src/services/AuthServices/AuthService.export.tsx
@@ -1,20 +1,5 @@
 import AuthService from "./AuthService";
 
-export const logout = () : Promise<any[] | null> => {
-    return new Promise ((resolve, reject) => {
-        AuthService.logout()
-            .then((response) => {
-                const apiData: any = response;
-                resolve(apiData);
-            })
-            .catch((err) => {
-                console.error(err);
-                // resolve(null);
-                reject(err);
-            })
-    })
-}
-
 export const signIn = (values:any) : Promise<any[] | null> => {
     return new Promise ((resolve, reject) => {
         AuthService.signIn(values)
@@ -43,5 +28,80 @@ export const signUp = (values:any) : Promise<any[] | null> => {
             // resolve(null);
             reject(error);
         })
+    })
+}
+
+export const logout = () : Promise<any[] | null> => {
+    return new Promise ((resolve, reject) => {
+        AuthService.logout()
+            .then((response) => {
+                const apiData: any = response;
+                resolve(apiData);
+            })
+            .catch((err) => {
+                console.error(err);
+                // resolve(null);
+                reject(err);
+            })
+    })
+}
+
+export const checkAuth = () : Promise<any[] | null> => {
+    return new Promise ((resolve, reject) => {
+        AuthService.checkAuth()
+            .then((response) => {
+                const apiData: any = response;
+                resolve(apiData);
+            })
+            .catch((err) => {
+                console.error(err);
+                // resolve(null);
+                reject(err);
+            })
+    })
+}
+
+export const changeRole = (userId: string, newRole: string) : Promise<any[] | null> => {
+    return new Promise ((resolve, reject) => {
+        AuthService.changeRole(userId, newRole)
+            .then((response) => {
+                const apiData: any = response;
+                resolve(apiData);
+            })
+            .catch((err) => {
+                console.error(err);
+                // resolve(null);
+                reject(err);
+            })
+    })
+}
+
+export const isAdmin = () : Promise<any[] | null> => {
+    return new Promise ((resolve, reject) => {
+        AuthService.isAdmin()
+            .then((response) => {
+                const apiData: any = response;
+                resolve(apiData);
+            })
+            .catch((err) => {
+                console.error(err);
+                // resolve(null);
+                reject(err);
+            })
+    })
+}
+
+export const isEditor = () : Promise<any[] | null> => {
+    return new Promise ((resolve, reject) => {
+        AuthService.isEditor()
+            .then((response) => {
+                const apiData: any = response;
+                resolve(apiData);
+            })
+            .catch((err) => {
+                console.error(err);
+                // resolve(null);
+                reject(err);
+            })
     })
 }
--- src/services/AuthServices/AuthService.tsx
+++ src/services/AuthServices/AuthService.tsx
@@ -2,21 +2,16 @@ import {RequestMethod} from "../../enums/RequestMethod";
 import { makeRequest } from "../../axios/ApiService";
 
 const endpoints: any = {
-    logout: '/auth/logout',
     signIn: '/auth/sign-in',
-    signUp: '/auth/sign-up'
+    signUp: '/auth/sign-up',
+    logout: '/auth/logout',
+    checkAuth: '/auth/check-auth',
+    changeRole: '/auth/change-role',
+    isAdmin: '/auth/is-admin',
+    isEditor: '/auth/is-editor',
 }
 
 class AuthService {
-    async logout(): Promise<{ data: any, success: boolean }> {        
-        return makeRequest(RequestMethod.POST, endpoints.logout)
-            .then(result => {
-                return result;
-            })
-            .catch(error => {
-                return error;
-            }); 
-    }
 
     async signIn(values:any): Promise<{ data: any, success: boolean }> {        
         return makeRequest(RequestMethod.POST, endpoints.signIn, {data:values})
@@ -30,6 +25,56 @@ class AuthService {
 
     async signUp(values:any): Promise<{ data:any, success:boolean}> {
         return makeRequest(RequestMethod.POST, endpoints.signUp, {data:values})
+            .then(result => {
+                return result;
+            })
+            .catch(error => {
+                return error;
+            });
+    }
+
+    async logout(): Promise<{ data: any, success: boolean }> {        
+        return makeRequest(RequestMethod.POST, endpoints.logout)
+            .then(result => {
+                return result;
+            })
+            .catch(error => {
+                return error;
+            }); 
+    }
+
+    async checkAuth(): Promise<{ data: any, success: boolean }> {
+        return makeRequest(RequestMethod.GET, endpoints.checkAuth)
+            .then(result => {
+                return result;
+            })
+            .catch(error => {
+                return error;
+            });
+    }
+
+    async changeRole(userId: string, newRole: string): Promise<{ data: any, success: boolean }> {
+        return makeRequest(RequestMethod.POST, endpoints.changeRole, {data:{ userId, newRole }})
+            .then(result => {
+                return result;
+            })
+            .catch(error => {
+                return error;
+            });
+    }
+
+    async isAdmin(): Promise<{ data: any, success: boolean }> {
+        return makeRequest(RequestMethod.GET, endpoints.isAdmin)
+            .then(result => {
+                return result;
+            })
+            .catch(error => {
+                return error;
+            });
+    }
+
+    async isEditor(): Promise<{ data: any, success: boolean }> {
+        return makeRequest(RequestMethod.GET, endpoints.isEditor)
             .then(result => {
                 return result;
             })
